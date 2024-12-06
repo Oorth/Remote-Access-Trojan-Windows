8326da43b3f7269c19b62dc8c52281a2c94bec59
Oorth <arthmaurya@gmail.com>
2024-12-06 23:24:34 +0530

    Added console

--- console.cpp
+++ console.cpp
@@ -13,6 +13,12 @@ int main() {
                  #+#    #+# #+#     #+#       #+#    #+# #+#     #+# #+#          
                 #########  ###     ###       ###    ### ###     ### ###           
 
+                                [::] Made By Oorth :) [::]
+                        
+                        [::] Use this Ethically, if you dont [::]
+                            [::] I'll just enjoy watching  [::]
+
+                                [::] UNDER DEVELOPMENT!! [::]
 )";
 
     return 0;
