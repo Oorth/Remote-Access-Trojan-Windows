24d9d63ece2a43028653c0cd0d1acaf2d75176c3
Oorth <arthmaurya@gmail.com>
2024-12-09 00:41:34 +0530

    this makes sense

--- client.cpp
+++ client.cpp
@@ -1,68 +1,39 @@
 #include <winsock2.h>
 #include <iostream>
-#include <string>
+#include <ws2tcpip.h>
 
-#pragma comment(lib, "ws2_32.lib")  // Link with ws2_32.lib
+using namespace std;
+#pragma comment(lib, "ws2_32.lib")
 
-int main() {
-    // Initialize Winsock
+int main()
+{
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
     WSADATA wsaData;
-    int wsaResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (wsaResult != 0) {
-        std::cerr << "WSAStartup failed with error: " << wsaResult << std::endl;
-        return 1;
+
+    
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    SOCKET clientSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    connect(clientSock, (sockaddr*)&serverAddr, sizeof(serverAddr));
+
+    const char* message = "Hello, Server!";
+    send(clientSock, message, strlen(message), 0);
+
+    char buffer[512];
+    int bytesReceived = recv(clientSock, buffer, sizeof(buffer), 0);
+    if (bytesReceived > 0)
+    {
+        buffer[bytesReceived] = '\0';
+        cout << "Received from server: " << buffer << endl;
     }
 
-    // Create a socket
-    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (clientSocket == INVALID_SOCKET) {
-        std::cerr << "Socket creation failed with error: " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return 1;
-    }
-
-    // Define the server address
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);  // Port number of the server
-    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");  // Localhost IP address
-
-    // Connect to the server
-    int connectResult = connect(clientSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress));
-    if (connectResult == SOCKET_ERROR) {
-        std::cerr << "Connect failed with error: " << WSAGetLastError() << std::endl;
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    std::cout << "Connected to server!" << std::endl;
-
-    // Send data to the server
-    std::string message = "Hello from the client!";
-    int sendResult = send(clientSocket, message.c_str(), message.length(), 0);
-    if (sendResult == SOCKET_ERROR) {
-        std::cerr << "Send failed with error: " << WSAGetLastError() << std::endl;
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Receive data from the server
-    char recvBuffer[512];
-    int recvResult = recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-    if (recvResult > 0) {
-        recvBuffer[recvResult] = '\0';  // Null-terminate the received data
-        std::cout << "Received from server: " << recvBuffer << std::endl;
-    } else if (recvResult == 0) {
-        std::cout << "Connection closed by server." << std::endl;
-    } else {
-        std::cerr << "Recv failed with error: " << WSAGetLastError() << std::endl;
-    }
-
-    // Cleanup
-    closesocket(clientSocket);
+    closesocket(clientSock);
     WSACleanup();
-
+    
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,85 +1,48 @@
-#include <winsock2.h>
 #include <iostream>
-#include <string>
+#include <winsock2.h>
+#include <windows.h>
 
-#pragma comment(lib, "ws2_32.lib")  // Link with ws2_32.lib
+using namespace std;
+#pragma comment(lib, "ws2_32.lib")                                                                          // Link with ws2_32.lib
 
-int main() {
-    // Initialize Winsock
+int main()
+{
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);                                                                      // Port 8080
+    serverAddr.sin_addr.s_addr = INADDR_ANY;                                                                // Any available network interface
+    
     WSADATA wsaData;
-    int wsaResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (wsaResult != 0) {
-        std::cerr << "WSAStartup failed with error: " << wsaResult << std::endl;
-        return 1;
+
+    
+    WSAStartup(MAKEWORD(2, 2), &wsaData);                                                                   // Initialize Winsock
+    
+    SOCKET serversock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);                                                // Make socket
+    int bindResult = bind(serversock, (sockaddr*)&serverAddr, sizeof(serverAddr));                                // Bind
+
+
+    listen(serversock, SOMAXCONN);
+    cout << "Waiting for incoming connections..." << std::endl;
+
+    
+    SOCKET clientSocket = accept(serversock, nullptr, nullptr);
+    cout << "Client connected!" << std::endl;
+
+    char buffer[512];
+    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesReceived > 0) {
+        buffer[bytesReceived] = '\0';
+        cout << "Received: " << buffer << endl;
+
+        // Echo the message back to the client
+        send(clientSocket, buffer, bytesReceived, 0);
     }
 
-    // Create a socket
-    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (serverSocket == INVALID_SOCKET) {
-        std::cerr << "Socket creation failed with error: " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return 1;char buffer[512];
-    }
-
-    // Bind the socket to an address and port
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(8080);
-
-    int bindResult = bind(serverSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress));
-    if (bindResult == SOCKET_ERROR) {
-        std::cerr << "Bind failed with error: " << WSAGetLastError() << std::endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Listen for incoming connections
-    int listenResult = listen(serverSocket, 5);  // Max backlog queue size = 5
-    if (listenResult == SOCKET_ERROR) {
-        std::cerr << "Listen failed with error: " << WSAGetLastError() << std::endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    std::cout << "Server is listening on port 8080..." << std::endl;
-
-    // Accept an incoming connection
-    SOCKET clientSocket = accept(serverSocket, NULL, NULL);
-    if (clientSocket == INVALID_SOCKET) {
-        std::cerr << "Accept failed with error: " << WSAGetLastError() << std::endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    std::cout << "Client connected!" << std::endl;
-
-    // Receive data from the client
-    char recvBuffer[512];
-    int recvResult = recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-    if (recvResult > 0) {
-        recvBuffer[recvResult] = '\0';  // Null-terminate the received data
-        std::cout << "Received: " << recvBuffer << std::endl;
-    } else if (recvResult == 0) {
-        std::cout << "Connection closed by client." << std::endl;
-    } else {
-        std::cerr << "Recv failed with error: " << WSAGetLastError() << std::endl;
-    }
-
-    // Send data to the client
-    std::string message = "Hello from the server!";
-    int sendResult = send(clientSocket, message.c_str(), message.length(), 0);
-    if (sendResult == SOCKET_ERROR) {
-        std::cerr << "Send failed with error: " << WSAGetLastError() << std::endl;
-    }
-
-    // Cleanup
     closesocket(clientSocket);
-    closesocket(serverSocket);
-    WSACleanup();
+    closesocket(serversock);
 
+    // Clean up Winsock before exiting
+    WSACleanup();
     return 0;
 }
