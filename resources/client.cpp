75b14d19cc4f3a5931b1bfe489c13e2aff46b5a2
Oorth <arthmaurya@gmail.com>
2024-12-09 14:24:57 +0530

    before pipes implementation

--- resources/client.cpp
+++ resources/client.cpp
@@ -5,6 +5,30 @@
 using namespace std;
 #pragma comment(lib, "ws2_32.lib")
 
+string receive_data(SOCKET clientSocket)
+{
+    char buffer[512];                                                                           // Buffer to store received data
+    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+
+    if (bytesReceived > 0)
+    {
+        buffer[bytesReceived] = '\0';                                                           // Null-terminate the string
+        string receivedData(buffer);                                                            // Store the data in a string
+        return receivedData;
+    }
+    else if (bytesReceived == 0) cerr << "Connection closed by server." << std::endl;
+    else cerr << "Receive failed with error: " << WSAGetLastError() << std::endl;
+
+    return "";                                                                                  // Return an empty string in case of error or connection closure
+}
+void send_data(SOCKET clientSocket, const string &data)                                                     // Doesnot send special data 
+{
+    int bytesSent = send(clientSocket, data.c_str(), data.length(), 0);
+    
+    if (bytesSent == SOCKET_ERROR) cerr << "Send failed with error: " << WSAGetLastError() << endl;
+    else cout << "Sent data: " << data << endl;
+}
+
 int main()
 {
 
@@ -18,19 +42,14 @@ int main()
     
     WSAStartup(MAKEWORD(2, 2), &wsaData);
     SOCKET clientSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
     connect(clientSock, (sockaddr*)&serverAddr, sizeof(serverAddr));
 
-    const char* message = "Hello, Server!";
-    send(clientSock, message, strlen(message), 0);
+    // const char* message = "Hello, Server!";
+    // send(clientSock, message, strlen(message), 0);
 
-    char buffer[512];
-    int bytesReceived = recv(clientSock, buffer, sizeof(buffer), 0);
-    if (bytesReceived > 0)
-    {
-        buffer[bytesReceived] = '\0';
-        cout << "Received from server: " << buffer << endl;
-    }
+    cout << "got this -> " << receive_data(clientSock) << endl;
+    send_data(clientSock,"Ye lo");
+
 
     closesocket(clientSock);
     WSACleanup();
--- resources/server.cpp
+++ resources/server.cpp
@@ -3,7 +3,31 @@
 #include <windows.h>
 
 using namespace std;
-#pragma comment(lib, "ws2_32.lib")                                                                          // Link with ws2_32.lib
+#pragma comment(lib, "ws2_32.lib");                                                                         // Link with ws2_32.lib
+
+void send_data(SOCKET clientSocket, const string &data)                                                     // Doesnot send special data 
+{
+    int bytesSent = send(clientSocket, data.c_str(), data.length(), 0);
+    
+    if (bytesSent == SOCKET_ERROR) cerr << "Send failed with error: " << WSAGetLastError() << endl;
+    else cout << "Sent data: " << data << endl;
+}
+string receive_data(SOCKET clientSocket)
+{
+    char buffer[512];                                                                           // Buffer to store received data
+    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+
+    if (bytesReceived > 0)
+    {
+        buffer[bytesReceived] = '\0';                                                           // Null-terminate the string
+        string receivedData(buffer);                                                            // Store the data in a string
+        return receivedData;
+    }
+    else if (bytesReceived == 0) cerr << "Connection closed by server." << std::endl;
+    else cerr << "Receive failed with error: " << WSAGetLastError() << std::endl;
+
+    return "";                                                                                  // Return an empty string in case of error or connection closure
+}
 
 int main()
 {
@@ -18,32 +42,24 @@ int main()
     
     WSAStartup(MAKEWORD(2, 2), &wsaData);                                                                   // Initialize Winsock
     
-    SOCKET serversock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);                                                // Make socket
-    int bindResult = bind(serversock, (sockaddr*)&serverAddr, sizeof(serverAddr));                                // Bind
+    SOCKET serversock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);                                          // Make socket
+    int bindResult = bind(serversock, (sockaddr*)&serverAddr, sizeof(serverAddr));                          // Bind
 
 
     listen(serversock, SOMAXCONN);
-    cout << "Waiting for incoming connections..." << std::endl;
+    cout << "Waiting for incoming connections..." << endl;
 
     
     SOCKET clientSocket = accept(serversock, nullptr, nullptr);
-    cout << "Client connected!" << std::endl;
+    cout << "Client connected!" << endl;
 
-    char buffer[512];
-    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (bytesReceived > 0)
-    {
-        buffer[bytesReceived] = '\0';
-        cout << "Received: " << buffer << endl;
-
-        // Echo the message back to the client
-        send(clientSocket, buffer, bytesReceived, 0);
-    }
+    send_data(clientSocket," Gib Shell pls UwU ");
+    cout << "got this -> " << receive_data(clientSocket) << endl;
+    
 
     closesocket(clientSocket);
     closesocket(serversock);
 
-    // Clean up Winsock before exiting
     WSACleanup();
     return 0;
 }
